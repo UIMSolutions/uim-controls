b4e1a2247e2e86c3c0562ab33c474e073ed4970e
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-24 18:06:33 +0200

    Updates

--- source/uim/controls/carousels/carousel.d
+++ source/uim/controls/carousels/carousel.d
@@ -9,6 +9,8 @@ class DUIMCarouselControl : DUIMControl {
   mixin(OProperty!("DUIMCarouselItemControl[]", "items"));
   mixin(OProperty!("bool", "showControls"));
   mixin(OProperty!("bool", "showIndicators"));
+  mixin(OProperty!("bool", "showDots"));
+  mixin(OProperty!("bool", "showThumbs"));
 
   override void initialize() {
     super.initialize;
@@ -24,20 +26,27 @@ class DUIMCarouselControl : DUIMControl {
     auto myAttributes = this.attributes.dup;
 
     auto indicatorCounter = 0;
+
+    auto myIndicators = BS5CarouselIndicators;
+    if (showIndicators) {
+      myIndicators.content(BS5CarouselIndicators(
+        items.map!(item => 
+          cast(DH5Obj)H5Button(
+            [item.active ? "active" : null], 
+            ["type":"button", "data-bs-target":"#"~myId, "data-bs-slide-to":to!string(++indicatorCounter), 
+            "aria-current":"true", "aria-label":"Slide "~to!string(indicatorCounter)])
+          ).array));
+
+      if (showDots) { myIndicators.addClasses("carousel-indicators-dot"); }
+      else if (showThumbs) { myIndicators.addClasses("carousel-indicators-thumbs"); }
+    }
+
     return [
       BS5Carousel(myId, myClasses, myAttributes, 
         (items  ? BS5CarouselInner(items.map!(item => item.toH5).join) 
                 : null),
-        ( showIndicators  ? BS5CarouselIndicators(items.map!(item => 
-            cast(DH5Obj)H5Button([item.active ? "active" : null], 
-            ["type":"button", "data-bs-target":"#"~myId, "data-bs-slide-to":to!string(++indicatorCounter), 
-              "aria-current":"true", "aria-label":"Slide "~to!string(indicatorCounter)])).array) 
-                          : null),
-        ( showControls  ? H5A(["carousel-control-prev"], ["href":"#carousel-sample", "role":"button", "data-bs-slide":"prev"], 
-                            H5Span(["carousel-control-prev-icon"], ["aria-hidden":"true"]),
-                            H5Span(["visually-hidden"], "Previous")) 
-                          : null),                  
-        ( showControls  ? H5A(["carousel-control-next"], ["href":"#carousel-sample", "role":"button", "data-bs-slide":"next"],
+        ( showIndicators ? myIndicators : null),
+        ( showControls  ? H5A(["carousel-control-next"], ["href":"#"~myId, "role":"button", "data-bs-slide":"next"],
                             H5Span(["carousel-control-next-icon"], ["aria-hidden":"true"]),
                             H5Span(["visually-hidden"], "Next")) 
                           : null)                  
