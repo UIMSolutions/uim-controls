a2ca8403d7499a434be4cca4d64001af07f523b6
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-25 18:12:38 +0200

    Update icon.d

--- source/uim/controls/empties/icon.d
+++ source/uim/controls/empties/icon.d
@@ -9,5 +9,16 @@ class DUIMEmptyIconControl : DUIMControl {
   override void initialize() {
     super.initialize;
   }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    string myId = this.id.dup;
+    auto myClasses = this.classes.dup;
+    auto myAttributes = this.attributes.dup;
+    auto myContent = this.content.dup;
+
+    return [
+      H5Div(myId, myClasses, myAttributes, myContent)
+    ].toH5;
+  }
 }
 auto UIMEmptyIconControl() { return new DUIMEmptyIconControl; }
