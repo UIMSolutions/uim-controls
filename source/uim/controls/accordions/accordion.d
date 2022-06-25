cedc5f0a93c1d0e83cbaf01abb6c2fe45308d112
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-25 20:58:29 +0200

    Updates

--- source/uim/controls/accordions/accordion.d
+++ source/uim/controls/accordions/accordion.d
@@ -2,3 +2,12 @@ module uim.controls.accordions.accordion;
 
 @safe:
 import uim.controls;
+
+class DUIMAccordionControl : DUIMControl {
+  this() { super(); }
+
+  override void initialize() {
+    super.initialize;
+  }
+}
+auto UIMAccordionControl() { return new DUIMAccordionControl; }
--- source/uim/controls/popovers/popover.d
+++ source/uim/controls/popovers/popover.d
@@ -2,3 +2,12 @@ module uim.controls.popovers.popover;
 
 @safe:
 import uim.controls;
+
+class DUIMPopoverControl : DUIMControl {
+  this() { super(); }
+
+  override void initialize() {
+    super.initialize;
+  }
+}
+auto UIMPopoverControl() { return new DUIMPopoverControl; }
