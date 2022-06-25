76a0f88a5b73fec307e2a78e8b598b6d5c76c4c0
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-25 18:13:38 +0200

    Update breadcrumb.d

--- source/uim/controls/breadcrumbs/breadcrumb.d
+++ source/uim/controls/breadcrumbs/breadcrumb.d
@@ -13,5 +13,16 @@ class DUIMBreadcrumbControl : DUIMControl {
       .classes(["alert-link"])
       .attributes(["href":"#"]);
   }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    string myId = this.id.dup;
+    auto myClasses = this.classes.dup;
+    auto myAttributes = this.attributes.dup;
+    auto myContent = this.content.dup;
+
+    return [
+      H5Div(myId, myClasses, myAttributes, myContent)
+    ].toH5;
+  }
 }
 auto UIMBreadcrumbControl() { return new DUIMBreadcrumbControl; }
