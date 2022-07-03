5ae006527d2953abd5ccc86fc66fd80bbed6c39b
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-03 15:52:12 +0200

    Update item.d

--- source/uim/controls/accordions/item.d
+++ source/uim/controls/accordions/item.d
@@ -13,6 +13,10 @@ class DUIMAccordionItemControl : DUIMControl {
     super.initialize;
   }
 
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+  }
+
   override DH5Obj[] toH5(STRINGAA options = null) {
     auto results = super.toH5(options);
 
