50fc843d33ccb351c6977633337300758d80dc8a
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-03 16:06:12 +0200

    Update list.d

--- source/uim/controls/buttons/list.d
+++ source/uim/controls/buttons/list.d
@@ -3,28 +3,18 @@ module uim.controls.buttons.list;
 @safe: 
 import uim.controls;
 
-class DUIMButtonListControl : DUIMControl {
+class DUIMButtonListControl : DUIMContainerControl {
   this() { super(); }
-
-  protected DUIMControl[] _items;  
-  DUIMControl[] items(this O)() {
-    return _items;
-  }
-  O items(this O)(DUIMControl[] newItems) {
-    _items = newItems;
-    _items.each!(item => item.parent(this));
-    return cast(O)this;
-  }
-  O items(this O)(DUIMControl[] newItems...) { 
-    this.items(newItems); 
-    return cast(O)this;
-  }
 
   override void initialize() {
     super.initialize;
 
     this
       .classes(["btn-list"]);
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
   }
 
   override DH5Obj[] toH5(STRINGAA options = null) {
