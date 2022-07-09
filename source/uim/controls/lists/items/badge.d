0628895f4d2d01ce08940baaf324d819c98ebe32
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 08:16:07 +0200

    Update badge.d

--- source/uim/controls/lists/items/badge.d
+++ source/uim/controls/lists/items/badge.d
@@ -7,8 +7,26 @@ import uim.controls;
 class DUIMBadgeListItemControl : DUIMContainerControl {
   mixin(ControlThis!("UIMBadgeListItemControl"));
 
+  mixin(OProperty!("string", "badgePosition")); // default right. Other values are left and both
+  mixin(OProperty!("string", "badgeContent"));
+  mixin(OProperty!("string", "badgeColor"));
+
   override void initialize() {
     super.initialize;
+    
+    this
+      .addClasses(["d-flex", "justify-content-between", "align-items-center"]);
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    before.toH5(options);
+    
+    auto myBadge = BS5Badge.color(badgeColor).content(badgeContent);
+    switch(badgePosition) {
+      case "left": myContent = myBadge~this.content; break
+      case "both": myContent = myBadge~this.content~myBadge; break
+      default: myContent = myBadge~this.content; break
+    }
   }
 
   override DH5Obj[] toH5(STRINGAA options = null) {
