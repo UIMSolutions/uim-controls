b0e647577748f8309d9caacfa9b60ecc7644df78
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 08:05:49 +0200

    Update badge.d

--- source/uim/controls/lists/items/badge.d
+++ source/uim/controls/lists/items/badge.d
@@ -1,1 +1,19 @@
-mod
+module uim.controls.lists.items.badge;
+
+@safe: 
+import uim.controls;
+
+// Simple listitem with a badge at start or end or both. 
+class DUIMBadgeListItemControl : DUIMContainerControl {
+  mixin(ControlThis!("UIMBadgeListItemControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    return super.toH5(options);
+  }
+}
+mixin(ControlCalls!("UIMBadgeListItemControl", "DUIMBadgeListItemControl"));
+mixin(ControlCalls!("UIMBadgeListItem", "DUIMBadgeListItemControl"));
