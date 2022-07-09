31962b37537b6b133bfbe5a7e924cfc4273bb9c5
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 08:07:02 +0200

    Create avatar.d

---
+++ source/uim/controls/lists/items/avatar.d
@@ -0,0 +1,19 @@
+module uim.controls.lists.items.avatar;
+
+@safe: 
+import uim.controls;
+
+// Simple listitem with a avatar at start or end or both. 
+class DUIMAvatarListItemControl : DUIMContainerControl {
+  mixin(ControlThis!("UIMAvatarListItemControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    return super.toH5(options);
+  }
+}
+mixin(ControlCalls!("UIMAvatarListItemControl", "DUIMAvatarListItemControl"));
+mixin(ControlCalls!("UIMAvatarListItem", "DUIMAvatarListItemControl"));
