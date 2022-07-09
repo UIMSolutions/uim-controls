a01939eb524dcd5e3aa5f7fa498fa28af93edac2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 08:49:01 +0200

    Update link.d

--- source/uim/controls/paginations/link.d
+++ source/uim/controls/paginations/link.d
@@ -3,8 +3,32 @@ module uim.controls.paginations.link;
 @safe:
 import uim.controls;
 
+class DUIMPageLinkControl : DUIMControl {
+  mixin(ControlThis!("UIMPageLinkControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);    
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results;
+  }
+}
+mixin(ControlCalls!("UIMPageLinkControl", "DUIMPageLinkControl"));
+mixin(ControlCalls!("UIMPageLink", "DUIMPageLinkControl"));
+
 version(test_uim_controls) {
   unittest {
+    assert(UIMPageLink);
+    
+    auto control = UIMPageLink;
     // TODO
   }
 }
+
