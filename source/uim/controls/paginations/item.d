1e7538c48326d48d312236a8bf0bac65ffa84f2e
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-09 08:47:46 +0200

    Update item.d

--- source/uim/controls/paginations/item.d
+++ source/uim/controls/paginations/item.d
@@ -3,6 +3,26 @@ module uim.controls.paginations.item;
 @safe:
 import uim.controls;
 
+class DUIMPageItemControl : DUIMControl {
+  mixin(ControlThis!("UIMPageItemControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);    
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results;
+  }
+}
+mixin(ControlCalls!("UIMPageItemControl", "DUIMPageItemControl"));
+mixin(ControlCalls!("UIMPageItem", "DUIMPageItemControl"));
+
 version(test_uim_controls) {
   unittest {
     // TODO
