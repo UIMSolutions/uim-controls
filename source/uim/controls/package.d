84692ca46a6a0d3bf88470d594398fabef70484b
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-19 06:23:40 +0200

    Working on calendar controls

--- source/uim/controls/package.d
+++ source/uim/controls/package.d
@@ -43,6 +43,7 @@ public import uim.controls.avatars;
 public import uim.controls.badges;
 public import uim.controls.breadcrumbs;
 public import uim.controls.buttons;
+public import uim.controls.calendars;
 public import uim.controls.cards;
 public import uim.controls.carousels;
 public import uim.controls.charts;
