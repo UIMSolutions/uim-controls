ce47174af84c5fb835a14118c65556080889a2dd
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-22 08:28:28 +0200

    Working on swirch-icon

--- source/uim/controls/switchicons/switchicon.d
+++ source/uim/controls/switchicons/switchicon.d
@@ -6,8 +6,32 @@ import uim.controls;
 class DUIMSwitchIconControl : DUIMControl {
   mixin(ControlThis!("UIMSwitchIconControl"));
 
+  mixin(OProperty!("string", "iconOne"));
+  mixin(OProperty!("string", "iconTwo"));
+
+  mixin(OProperty!("string[]", "classesOne"));
+  mixin(OProperty!("string[]", "classesTwo"));
+
   override void initialize() {
     super.initialize;
+
+    this
+      .classes(["switch-icon"])
+      .attributes(["data-bs-toggle":"switch-icon"]);
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results~
+      H5Button(myId, myClasses, myAttributes, 
+        H5Span(["switch-icon-a"]~classesOne, tablerIcon(iconOne)),
+        H5Span(["switch-icon-b"]~classesTwo, tablerIcon(iconTwo)));
   }
 }
 mixin(ControlCalls!("UIMSwitchIconControl", "DUIMSwitchIconControl"));
@@ -21,3 +45,5 @@ version(test_uim_controls) {
     // TODO
   }
 }
+
+
