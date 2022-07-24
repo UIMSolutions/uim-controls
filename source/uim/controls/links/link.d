762881d1f5f3f5f6c7be4505c753cd175fc3ac7c
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-24 09:43:07 +0200

    Update link.d

--- source/uim/controls/links/link.d
+++ source/uim/controls/links/link.d
@@ -1,1 +1,16 @@
 module uim.controls.links.link;
+
+/*
+Ein Hyperlink-Steuerelement zum Navigieren zu anderen Apps und Webseiten oder zum Auslösen von Aktionen.
+
+ Überblick
+ Das Link-Control ist ein anklickbares Textelement, das so visualisiert wird, dass es sich vom Standardtext abhebt.  Beim Hover ändert es seinen Stil in unterstrichenen Text, um dem Benutzer zusätzliches Feedback zu geben.
+
+ Verwendungszweck
+ Sie können den Link aktivieren oder deaktivieren.
+
+ Um eine visuelle Hierarchie in großen Linklisten zu erstellen, können Sie die weniger wichtigen Links dezent oder die wichtigeren hervorheben.
+
+ Um anzugeben, wo der verknüpfte Inhalt geöffnet wird, können Sie die Eigenschaft target verwenden.
+
+*/
