29f90709eea4215cef486ee454cfd5417ccaae18
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-31 11:36:30 +0200

    Update image.d

--- source/uim/controls/placeholders/image.d
+++ source/uim/controls/placeholders/image.d
@@ -1,1 +1,23 @@
 module uim.controls.placeholders.image;
+
+@safe: 
+import uim.controls;
+
+class DUIMPlaceHolderImageControl : DUIMControl {
+  mixin(ControlThis!("UIMPlaceHolderImageControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+}
+mixin(ControlCalls!("UIMPlaceHolderImageControl", "DUIMPlaceHolderImageControl"));
+mixin(ControlCalls!("UIMPlaceHolderImage", "DUIMPlaceHolderImageControl"));
+
+version(test_uim_controls) {
+  unittest {
+    assert(UIMPlaceHolderImage);
+
+    auto control = UIMPlaceHolderImage;
+    // TODO
+  }
+}
