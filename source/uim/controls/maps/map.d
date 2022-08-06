17f3b3d800567d9ebf0c2db0340bc54f144de0b9
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-06 20:38:07 +0200

    Updates

--- source/uim/controls/maps/map.d
+++ source/uim/controls/maps/map.d
@@ -1,1 +1,34 @@
 module uim.controls.maps.map;
+
+@safe: 
+import uim.controls;
+
+class DUIMMapControl : DUIMControl {
+  mixin(ControlThis!("UIMMapControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results~
+      H5Div(myId, myClasses, myAttributes, myContent);
+  }
+}
+mixin(ControlCalls!("UIMMapControl", "DUIMMapControl"));
+mixin(ControlCalls!("UIMMap", "DUIMMapControl"));
+
+version(test_uim_controls) {
+  unittest {
+    assert(UIMMap);
+
+    auto control = UIMMap;
+  }
+}
