0fc01026ed59b915f438370a1171ba98c1efd0c2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-06 09:24:25 +0200

    Create content.d

---
+++ source/uim/controls/datagrids/content.d
@@ -0,0 +1,31 @@
+module uim.controls.datagrids.content;
+
+@safe: 
+import uim.controls;
+
+class DUIMDatagridContentControl : DUIMControl {
+  mixin(ControlThis!("UIMDatagridContentControl"));
+
+  override void initialize() {
+    super.initialize;
+
+    this
+      .classes(["datagrid-content"]);
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results;  
+  }
+}
+mixin(ControlCalls!("UIMDatagridContentControl", "DUIMDatagridContentControl"));
+mixin(ControlCalls!("UIMDatagridContent", "DUIMDatagridContentControl"));
+
+version(test_uim_controls) {
+  unittest {
+    assert(UIMDatagridContent);
+
+    auto control = UIMDatagridContent;
+  }
+}
