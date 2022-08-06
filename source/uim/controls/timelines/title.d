7a2fa71ffeb12102c4471697b63f6a604af847d6
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-06 07:58:49 +0200

    Update title.d

--- source/uim/controls/timelines/title.d
+++ source/uim/controls/timelines/title.d
@@ -1,31 +1,31 @@
-list-timeline-title
+module uim.controls.timelines.title;
+
 @safe: 
 import uim.controls;
 
-// A LinkControl is a hyperlink control which is used to navigate to other pages or to trigger actions.
-class DUIMLinkControl : DUIMControl {
-  mixin(ControlThis!("UIMLinkControl"));
+class DUIMTimelineTitleControl : DUIMControl {
+  mixin(ControlThis!("UIMTimelineTitleControl"));
 
   override void initialize() {
     super.initialize;
 
-    this.classes(["list-timeline-icon"]);
+    this.classes(["list-timeline-title"]);
   }
 
   override DH5Obj[] toH5(STRINGAA options = null) {
     auto results = super.toH5(options);
 
     return results~
-      H5Div(myId, myClasses, myAttributes, myContent);
+      H5P(myId, myClasses, myAttributes, myContent);
   }
 }
-mixin(ControlCalls!("UIMLinkControl", "DUIMLinkControl"));
-mixin(ControlCalls!("UIMLink", "DUIMLinkControl"));
+mixin(ControlCalls!("UIMTimelineTitleControl", "DUIMTimelineTitleControl"));
+mixin(ControlCalls!("UIMTimelineTitle", "DUIMTimelineTitleControl"));
 
 version(test_uim_controls) {
   unittest {
-    assert(UIMLink);
+    assert(UIMTimelineTitle);
 
-    auto control = UIMLink;
+    auto control = UIMTimelineTitle;
   }
 }
