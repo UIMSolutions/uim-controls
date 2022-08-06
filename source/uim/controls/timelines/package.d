451e21eb1976858160d15e59052692f7002cbf85
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-06 07:53:57 +0200

    Update package.d

--- source/uim/controls/timelines/package.d
+++ source/uim/controls/timelines/package.d
@@ -7,4 +7,5 @@ public import uim.controls.timelines.timeline;
 public import uim.controls.timelines.content;
 public import uim.controls.timelines.icon;
 public import uim.controls.timelines.time;
+public import uim.controls.timelines.title;
 
