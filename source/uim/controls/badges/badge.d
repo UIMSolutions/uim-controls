71173175433ca4ceffb34d9607f41ba6fb33affb
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-06 20:45:56 +0200

    Add tests

--- source/uim/controls/badges/badge.d
+++ source/uim/controls/badges/badge.d
@@ -16,6 +16,19 @@ class DUIMBadgeControl : DUIMControl {
     return cast(O)this;
   }
 
+  O value(this O)(string newValue) {
+    content(newValue);
+    return cast(O)this;
+  }
+  O value(this O)(int newValue) {
+    content(to!string(newValue));
+    return cast(O)this;
+  }
+  O value(this O)(double newValue) {
+    content(to!string(newValue));
+    return cast(O)this;
+  }
+
   override void initialize() {
     super.initialize;
 
@@ -25,16 +38,16 @@ class DUIMBadgeControl : DUIMControl {
 
   override void beforeH5(STRINGAA options = null) {
     super.beforeH5(options);
-  }
-
-  override DH5Obj[] toH5(STRINGAA options = null) {
-    DH5Obj[] results = super.toH5(options);
 
     if (color && !outline) { myClasses ~= "bg-"~this.color; }
     if (color && outline) { myClasses ~= ["text-"~this.color, "badge-outline"]; }
     if (style) { myClasses ~= "badge-"~style; }
-    
     if (link) { myAttributes["href"] = link; }
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    DH5Obj[] results = super.toH5(options);
+    
     return results~
       (link  ? H5A(myId, myClasses, myAttributes, myContent)
              : H5Span(myId, myClasses, myAttributes, myContent));
@@ -48,4 +61,10 @@ version(test_uim_controls) {
     auto control = UIMBadge;
    // TODO
   }
+}
+unittest {
+  writeln(UIMBadge.id("test").color("testColor"));
+  writeln(UIMBadge.id("test").link("testLink"));
+  writeln(UIMBadge.id("test").outline(true));
+  writeln(UIMBadge.id("test").style("testStyle"));
 }
