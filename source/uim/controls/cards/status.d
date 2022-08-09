b0bb7f9dc5ee979434728cb33b43f730191d49c2
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-08-09 17:44:56 +0200

    Add Card status

---
+++ source/uim/controls/cards/status.d
@@ -0,0 +1,36 @@
+module uim.controls.cards.status;
+
+@safe: 
+import uim.controls;
+
+class DUIMCardStatusControl : DUIMControl {
+  mixin(ControlThis!("UIMCardStatusControl"));
+
+  override void initialize() {
+    super.initialize;
+
+    this
+      .classes(["card-status"]);
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results~
+      H5Div(myId, myClasses, myAttributes, myContent);  
+  }
+}
+mixin(ControlCalls!("UIMCardStatusControl", "DUIMCardStatusControl"));
+mixin(ControlCalls!("UIMCardStatus", "DUIMCardStatusControl"));
+
+version(test_uim_controls) {
+  unittest {
+    assert(UIMCardStatus);
+
+    auto control = UIMCardStatus;
+  }
+}
