6e1f6d6bb41d1420ac34edf9eff3d92da939d896
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-08-09 17:21:44 +0200

    Adding slider

--- source/uim/controls/sliders/slider.d
+++ source/uim/controls/sliders/slider.d
@@ -1,1 +1,33 @@
 module uim.controls.sliders.slider;
+
+@safe: 
+import uim.controls;
+
+class DUIMSliderControl : DUIMControl {
+  mixin(ControlThis!("UIMSliderControl"));
+
+  override void initialize() {
+    super.initialize;
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    auto results = super.toH5(options);
+
+    return results~
+      H5Div(myId, myClasses, myAttributes, myContent);
+  }
+}
+mixin(ControlCalls!("UIMSliderControl", "DUIMSliderControl"));
+mixin(ControlCalls!("UIMSlider", "DUIMSliderControl"));
+
+version(test_uim_controls) {
+  unittest {
+    assert(UIMSlider);
+
+    auto control = UIMSlider;
+  }
+}
