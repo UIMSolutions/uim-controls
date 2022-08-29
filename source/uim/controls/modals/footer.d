d415edbd0dc906504c91dace889443886af90bc2
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-08-29 09:56:05 +0200

    FEAT: Working on modals

--- source/uim/controls/modals/footer.d
+++ source/uim/controls/modals/footer.d
@@ -21,8 +21,7 @@ class DUIMModalFooterControl : DUIMControl {
   override DH5Obj[] toH5(STRINGAA options = null) {
     auto results = super.toH5(options);
 
-    return results~
-      H5Div(myId, myClasses, myAttributes, myContent);
+    return [H5Div(myId, myClasses, myAttributes, myContent)].toH5;
   }
 }
 mixin(ControlCalls!("UIMModalFooterControl", "DUIMModalFooterControl"));
