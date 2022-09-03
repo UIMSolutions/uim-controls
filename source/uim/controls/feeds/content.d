385d588bf2b3818df70dec6e339794b21697bf9f
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-03 09:01:04 +0200

    Update content.d

--- source/uim/controls/feeds/content.d
+++ source/uim/controls/feeds/content.d
@@ -1,1 +1,31 @@
-module uim.controls.feeds.content.d
+module uim.controls.feeds.content;
+
+@safe:
+import uim.controls;
+
+class DUIMBarControl : DUIMControl {
+  mixin(ControlThis!("UIMBarControl"));
+  
+  override void initialize() {
+    super.initialize;
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    super.toH5(options);
+    
+    return [
+      H5Div(myId, myClasses, myAttributes, myContent)
+    ].toH5;
+  }
+}
+mixin(ControlCalls!("UIMBarControl", "DUIMBarControl"));
+mixin(ControlCalls!("UIMBar", "DUIMBarControl"));
+
+version(test_uim_controls) { unittest {
+    assert(UIMBar);
+  }
+}
