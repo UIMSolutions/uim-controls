17b71ed68c25f80f2c58ee163bb34ac619f37d99
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-10 16:11:13 +0200

    Update layout.d

--- source/uim/controls/carousels/layout.d
+++ source/uim/controls/carousels/layout.d
@@ -1,2 +1,23 @@
 module uim.controls.carouseks.layout;
 
+/*
+
+Applies a sap.m.CarouselLayout to a provided DOM element or Control.
+
+Constructor
+Accepts an object literal mSettings that defines initial property values, aggregated and associated objects as well as event handlers. See sap.ui.base.ManagedObject#constructor for a general description of the syntax of the settings object.
+
+Name
+visiblePagesCount
+Type:
+int
+Default Value:
+1
+Description:
+Defines how many pages are displayed in the visible area of the Carousel control. Value should be a positive number.
+
+Note: When this property is set to something different than the default value, the loop property of Carousel is ignored.
+
+Visibility: publicEntry validated successfully
+
+*/
