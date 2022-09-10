7b225a13cb970dbf5aecd3af09c2c05a7394a064
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-10 16:06:56 +0200

    Update colorpalette.d

--- source/uim/controls/colorpalettes/colorpalette.d
+++ source/uim/controls/colorpalettes/colorpalette.d
@@ -1,1 +1,30 @@
 module uim.controls.colorpalettes.colorpalette;
+
+@safe: 
+import uim.controls;
+
+class DUIMColorPaletteControl : DUIMControl {
+  mixin(ControlThis!("UIMColorPaletteControl"));
+
+  override void initialize() {
+    super.initialize;
+
+    this
+      .classes(["color-palette"]);
+  }
+
+  // Rendering
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    super.toH5(options);
+
+    return [
+      H5Div(myId, myClasses, myAttributes, myContent)].toH5;
+  }
+}
+mixin(ControlCalls!("UIMColorPaletteControl", "DUIMColorPaletteControl); 
+mixin(ControlCalls!("UIMColorPalette", "DUIMColorPaletteControl); 
+
+version(test_uim_controls) { unittest {
+  assert(UIMColorPalette);
+  assert(UIMColorPalette.noId == `<div class="color-palette"></div>`);
+}}
