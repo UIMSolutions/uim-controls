1cd84f266f4e033d714688c0d1b795eb051a460a
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-09-11 10:03:59 +0200

    Update label.d

--- source/uim/controls/labels/label.d
+++ source/uim/controls/labels/label.d
@@ -1,1 +1,36 @@
 module uim.controls.labels.label;
+
+@safe: 
+import uim.controls;
+
+class DUIMLabelControl : DUIMControl {
+  mixin(ControlThis!("UIMLabelControl"));
+
+  mixin(OProperty!("string", "link"));
+
+  override void initialize() {
+    super.initialize;
+
+    this
+      .classes("label");
+  }
+
+  override void beforeH5(STRINGAA options = null) {
+    super.beforeH5(options);
+
+    if (link) myAttributes["href"] = link;
+  }
+
+  override DH5Obj[] toH5(STRINGAA options = null) {
+    super.toH5(options);
+
+    return [H5Label(myId, myClasses, myAttributes, myContent)].toH5;
+  }
+}
+mixin(ControlCalls!("UIMLabelControl", "DUIMLabelControl"));
+mixin(ControlCalls!("UIMLabel", "DUIMLabelControl")"));
+
+version(test_uim_controls) { unittest {
+    assert(UIMLabel);
+    assert(UIMLabel.noId == '<label class="label"><label>');
+}}
