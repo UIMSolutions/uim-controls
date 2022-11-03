c7f96736c6dd58d676e67bfe3d603d5cb33a3cd5
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-03 08:41:46 +0100

    Updates

--- source/uim/controls/bars/bar.d
+++ source/uim/controls/bars/bar.d
@@ -3,15 +3,18 @@ module uim.controls.bars.bar;
 @safe:
 import uim.controls;
 
-class DUIMBarControl : DUIMControl {
+class DUIMBarControl : DUIMDivControl {
   mixin(ControlThis!("UIMBarControl"));
   
   override void initialize() {
     super.initialize;
+
+    this
+      .classes("bar");
   }
 
   // Determines the design of the bar. If set to auto, it becomes dependent on the place where the bar is placed.
-  mixin(OProperty!("UIMBarDesigns", "design")); // DEfault Auto
+  mixin(OProperty!("UIMBarDesigns", "design")); // Default Auto
 
   // Specifies the Title alignment (theme specific). If set to TitleAlignment.None, the automatic title alignment depending on the theme settings will be disabled. If set to TitleAlignment.Auto, the Title will be aligned as it is set in the theme (if not set, the default value is center); Other possible values are TitleAlignment.Start (left or right depending on LTR/RTL), and TitleAlignment.Center (centered)
   mixin(OProperty!("UIMTitleAlignments", "titleAlignment")); // Default None
@@ -19,19 +22,11 @@ class DUIMBarControl : DUIMControl {
   override void beforeH5(STRINGAA options = null) {
     super.beforeH5(options);
   }
-
-  override DH5Obj[] toH5(STRINGAA options = null) {
-    super.toH5(options);
-    
-    return [
-      H5Div(myId, myClasses, myAttributes, myContent)
-    ].toH5;
-  }
 }
 mixin(ControlCalls!("UIMBarControl", "DUIMBarControl"));
 mixin(ControlCalls!("UIMBar", "DUIMBarControl"));
 
 version(test_uim_controls) { unittest {
-    assert(UIMBar);
-  }
-}
+  assert(UIMBar);
+  assert(UIMBar == `<div class="bar"></div>`);
+}}
