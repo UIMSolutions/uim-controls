3b977357d202bb2c843c665e3bd934eabd02c5e9
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-29 10:42:44 +0200

    Updates

--- source/uim/controls/dropdowns/menus/package.d
+++
@@ -1,3 +0,0 @@
-module uim.controls.dropdowns.menus;
-
-public import uim.controls.dropdowns.menus.menu;
--- source/uim/controls/dropzones/package.d
+++
@@ -1,3 +0,0 @@
-module uim.controls.dropzones;
-
-public import uim.controls.dropzones.dropzone;
